bd07fa4ba5f633aa916b57de1264d688d2d0df4b
smhabib99-99 <habibaust34@gmail.com>
2023-11-30 15:09:17 +0600

    users data send to data base creation is successful

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("bloodDb").collection("users");
+
+    // users related api
+    app.post('/users',async(req, res) =>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
